cb4c51a5dd71b1433f652bd7a090bb83a6eb4a77
git <stian.larsen@sparebank1.no>
2025-06-04 10:10:16 +0200

    Updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,7 +6,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: HOST,
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "daily",
       priority: 1,
     },
     {
